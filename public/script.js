4f827066c9ce44358e21d83775b142456e9d7a7c
tanmayidev <tanmayi.d.99@gmail.com>
2023-05-08 22:28:10 +0530

    add video stream and play video

--- public/script.js
+++ public/script.js
@@ -1,13 +1,40 @@
 const socket = io("/"); // socket connects to root path of our app
 
+const videoGrid = document.getElementById("video-grid");
+
 const myPeer = new Peer(undefined, {
   host: "/",
   port: "3001",
 });
 
-// sends event to server
-socket.emit("join-room", ROOM_ID, 10);
+const myVideo = document.createElement("video");
+myVideo.muted = true; // mutes our video for ourselves and not for others
 
+//connect video
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+  });
+
+// on "join-room" event (sends event to server)
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+// on "user-connected" event
 socket.on("user-connected", (userId) => {
   console.log("User connected " + userId);
 });
+
+// takes video and stream and plays the video , appends the video to stream
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
